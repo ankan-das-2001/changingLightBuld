8fd414ff6d2ae03f80e3bbe3e63219b3e8966b01
Ankan Das <ankandas1720@gmail.com>
2021-01-23 22:16:08 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+function torch(){
+    
+    document.getElementById("p1").innerHTML= '<p id="p2">yeh hai mentos zindegi</p>';
+    document.getElementById("img").innerHTML='<img src="onbulb.jpeg">'
+    console.log(12);
+}
